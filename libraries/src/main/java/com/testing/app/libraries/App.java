cc2c8e0ce8c14595d34762d37c9702b16f45c325
ssampathkumar104 <sampathkumargithub1410@gmail.com>
2021-07-05 21:56:48 +0530

    New project added to repo

---
+++ libraries/src/main/java/com/testing/app/libraries/App.java
@@ -0,0 +1,13 @@
+package com.testing.app.libraries;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
